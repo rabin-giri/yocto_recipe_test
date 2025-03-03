802f95bbbc8afdd5ebb18e94cfbcaf96ee0cf09c
Rabin Giri <75846015+rabin-giri@users.noreply.github.com>
2025-03-03 15:45:38 +0545

    Update hello_git.c

--- hello_git.c
+++ hello_git.c
@@ -1,1 +1,8 @@
-adfdfd
+//this program is just for yocto recipe test . this is very basic program to this hello world
+#include<stdio.h>
+
+int main()
+{
+  printf("hello world from github");
+  return 0;
+}
